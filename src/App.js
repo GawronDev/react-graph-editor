02d8d24b2a9aeb200f036d37054fdd8403eb8a5c
GawronDev <kubusgawron@gmail.com>
2022-12-26 16:43:02 +0100

    Added functioning relations

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ConnectDialog from "./components/ConnectDialog";
 import NodeElem from "./components/NodeElem";
 import Main from "./components/Main";
 import { nanoid } from "nanoid";
-import Xarrow, {useXarrow, Xwrapper} from "react-xarrows";
+import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
 
 // Alphabeth for node names
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -15,13 +15,15 @@ function resetAlphabet() {
     alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 }
 
+var relations = {};
+
 export default function App() {
-    console.log("App reseted");
+    // console.log("App reseted");
     const [openDialog, setOpenDialog] = useState(false);
-    const [relations, setRelations] = useState([]);
     const [nodes, setNodes] = useState([]);
 
     function clearNodes() {
+        relations = [];
         setNodes([]);
         resetAlphabet();
     }
@@ -31,13 +33,25 @@ export default function App() {
     }
 
     function connectNodes(parent, child) {
-        console.log(getNode(parent), getNode(child));
-        setNodes((prev) => {
-            return [
-                ...prev,
-                <Xarrow id="arrow" start={parent} end={child} color={"black"} headSize={3}/>
-            ]
-        })
+        for (var key in relations) {
+            if (key == parent) {
+                console.log("1");
+                if (relations[key].includes(child)) {
+                    console.log("Tried to duplicate a connection");
+                } else {
+                    var current_connections = relations[parent];
+                    current_connections.push(child);
+                    relations[parent] = current_connections;
+                    console.log(relations);
+                    setNodes((prev) => {
+                        return [
+                            ...prev,
+                            <Xarrow id="arrow" start={parent} end={child} color={"black"} headSize={3} curveness={0.8} />
+                        ];
+                    })
+                }
+            }
+        }
     }
 
     function getNode(node_name) {
@@ -54,6 +68,7 @@ export default function App() {
                 var name = alphabet[0];
                 alphabet.shift();
                 var id = nanoid();
+                relations[name] = [];
                 return [
                     ...prev,
                     <NodeElem name={name} key={id} id={name} parents={[]} children={[]} connected={false} />
@@ -61,20 +76,30 @@ export default function App() {
             } else {
                 nodeNum++;
                 var id = nanoid();
+                relations[nodeNum] = [];
                 return [
                     ...prev,
-                    <NodeElem name={nodeNum} key={id} id={name} parents={[]} children={[]} connected={false}/>
+                    <NodeElem name={nodeNum} key={id} id={name} parents={[]} children={[]} connected={false} />
                 ]
             }
         });
     }
 
+    function prepopulateGraph(){
+        console.log("Populating graph");
+    }
 
     return (
         <div className="general-wrapper">
             {openDialog ? <ConnectDialog closeDialog={openConnectDialog} connectNodes={connectNodes} nodes={nodes} updateNodes={setNodes} /> : ""}
             <Sidebar clear_nodes={clearNodes} new_node={addNode} openConnectDialog={openConnectDialog} />
             <Main nodes={nodes} clearNodes={clearNodes} addNode={addNode} />
+            <div id="debug" className="debug">
+                <p>
+                    {JSON.stringify(relations)};
+                </p>
+                <button onClick={prepopulateGraph}>Pre-populate graph</button>
+            </div>
         </div>
     );
 }
